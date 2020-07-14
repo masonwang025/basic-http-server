5341418e06f03541d47a3a460bd2575d12fb080c
MasonWang025 <mason.wang0025@gmail.com>
2020-07-13 18:36:33 -0700

    http server complete

--- index.js
+++ index.js
@@ -2,9 +2,81 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const serveHTMLPage = require("./serveHTMLPage");
 
 // http server
+const server = http.createServer((req, res) => {
+  // File path with ternary to check for /
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let extname = path.extname(filePath); // extension
+
+  // check extension and set content type
+  let contentType = "text/html";
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  if (contentType == "text/html" && extname == "") filePath += ".html";
+
+  // Read File
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        // file doesn't exist
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            if (err) {
+              // if there is no 404.html
+              res.writeHead(404);
+              res.end("404");
+            }
+            res.writeHead(404, {
+              "Content-Type": "text/html",
+            });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        // server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // file does exist
+      res.writeHead(200, {
+        "Content-Type": contentType,
+      });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+// let env var decide port
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+/*
+OLD CODE (using if else chain to handle routes):
 const server = http.createServer((req, res) => {
   // homepage
   if (req.url === "/") {
@@ -23,8 +95,27 @@ const server = http.createServer((req, res) => {
     serveHTMLPage(path.join("public", "404.html"), res); // serve 404 page
   }
 });
+*/
+/*
+CODE FOR serveHTMLPage:
+const path = require("path");
+const fs = require("fs");
 
-// let env var decide port
-const PORT = process.env.PORT || 5000;
+// takes in relative path the html file and res object
+const serveHTMLPage = (relativePath, res) => {
+  fs.readFile(path.join(__dirname, relativePath), (err, content) => {
+    if (err) {
+      // 404 isn't handled or 404.html doesn't exist
+      res.end("404! Cannot locate requested file.");
+    }
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    // res header of content type html
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(content);
+  });
+};
+
+module.exports = serveHTMLPage;
+*/
